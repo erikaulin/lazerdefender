a267374217732a1be238de5547370e8df1f8c100
Erik Aulin <erik@aulin.co>
2015-04-03 19:57:21 +0200

    Added Mathf.Camp to PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,13 +2,32 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-	public float speed = 1.0f;
+	public float speed = 15.0f;
+	public float padding = 1; 
+	
+	private float xmax = -5;
+	private float xmin = 5;
+	
+	void Start (){
+		Camera camera = Camera.main;
+		float distance = transform.position.z - camera.transform.position.z;
+		xmin = camera.ViewportToWorldPoint(new Vector3(0,0,distance)).x + padding;
+		xmax = camera.ViewportToWorldPoint(new Vector3(1,1,distance)).x	- padding;
+	}
 	
 	void Update () {
 		if(Input.GetKey(KeyCode.LeftArrow)){
-			transform.position += new Vector3(-speed, 0, 0);
+			transform.position = new Vector3(
+				Mathf.Clamp(transform.position.x - speed * Time.deltaTime, xmin, xmax),
+				 transform.position.y,
+				 transform.position.z
+			);
 		}else if (Input.GetKey(KeyCode.RightArrow)){
-			transform.position += new Vector3(+speed, 0,0);
+			transform.position = new Vector3(
+				Mathf.Clamp(transform.position.x + speed * Time.deltaTime, xmin, xmax),
+				transform.position.y,
+				transform.position.z
+			);
 		//}else if (Input.GetKey(KeyCode.UpArrow)){
 		//	transform.position += new Vector3(-speed, 0,0);
 		//}else if (Input.GetKey(KeyCode.DownArrow)){
@@ -16,3 +35,5 @@ public class PlayerController : MonoBehaviour {
 		}
 	}
 }
+
+
