7a0bb0552b1e4e918366130e0e51aa409e8b60aa
Erik Aulin <erik@ustwo.com>
2015-04-03 06:25:24 +0200

    Moved stuff around

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float speed = 1.0f;
+	
+	void Update () {
+		if(Input.GetKey(KeyCode.LeftArrow)){
+			transform.position += new Vector3(-speed, 0, 0);
+		}else if (Input.GetKey(KeyCode.RightArrow)){
+			transform.position += new Vector3(+speed, 0,0);
+		//}else if (Input.GetKey(KeyCode.UpArrow)){
+		//	transform.position += new Vector3(-speed, 0,0);
+		//}else if (Input.GetKey(KeyCode.DownArrow)){
+		//	transform.position += new Vector3(+speed, 0,0);
+		}
+	}
+}
