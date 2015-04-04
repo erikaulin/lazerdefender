f1b9f1164921b01c971d55bef5158c3b89d20c2e
Erik Aulin <erik@aulin.co>
2015-04-04 21:38:58 +0200

    pew pew scripts

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,12 +3,14 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 	public GameObject laser;
+	public float projectileSpeed = 10;
+	public float projectileRepeatRate = 0.2f;
 	
 	public float speed = 15.0f;
 	public float padding = 1; 
 	
-	private float xmax = -6;
-	private float xmin = 6;
+	private float xmax = -5;
+	private float xmin = 5;
 	
 	void Start (){
 		Camera camera = Camera.main;
@@ -17,9 +19,17 @@ public class PlayerController : MonoBehaviour {
 		xmax = camera.ViewportToWorldPoint(new Vector3(1,1,distance)).x	- padding;
 	}
 	
+	void Fire () {
+		GameObject beam = Instantiate(laser, transform.position, Quaternion.identity) as GameObject;
+		beam.rigidbody2D.velocity = new Vector3(0, projectileSpeed,0);
+	}
+	
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.Space)){
-			Instantiate(laser, transform.position, Quaternion.identity);
+		if(Input.GetKeyDown(KeyCode.Space)){
+			InvokeRepeating ("Fire", 0.0001f, projectileRepeatRate);
+		}
+		if(Input.GetKeyUp(KeyCode.Space)){
+			CancelInvoke("Fire");
 		}
 		if(Input.GetKey(KeyCode.LeftArrow)){
 			transform.position = new Vector3(
