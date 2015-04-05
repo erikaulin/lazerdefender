bf3ab82701079e2d5dc85576f8d307b1259324f9
Erik Aulin <erik@aulin.co>
2015-04-05 10:33:52 +0200

    Enemy destoyed on trigger

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -2,8 +2,18 @@
 using System.Collections;
 
 public class EnemyBehaviour : MonoBehaviour {
+	public float health = 150;
 
 	void OnTriggerEnter2D(Collider2D collider){
-		Debug.Log(collider);
+		//Debug.Log(collider);
+		Projectile missile = collider.gameObject.GetComponent<Projectile>();
+		if(missile){
+			//Debug.Log("Hit by a projectile");
+			health -= missile.GetDamage();
+			missile.Hit();
+			if (health <= 0) {
+				Destroy(gameObject);
+			}
+		}
 	}
 }
